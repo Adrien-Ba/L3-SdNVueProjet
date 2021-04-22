8c766bd2cfa64f3ba18920656082ec37d323c9ba
root <root@Yume-Pc.localdomain>
2021-04-22 14:03:26 +0200

    recuperation des données et base de la page

--- my-vue-app/src/components/PeuImporte.vue
+++ my-vue-app/src/components/PeuImporte.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
+    <input type="test" v-model="nomPkmn"/>
+    <table>
+      <tr>{{listeNomPkmn?.name}}</tr>
+      <tr>{{listeNomPkmn?.url}}</tr>
+    </table>
     {{ cards }}
+    <button @click="description">Toggle</button>
+    {{ pokemonDesc }}
   </div>
 </template>
 
@@ -10,21 +17,48 @@ import axios from 'axios';
 
 export default defineComponent({
   setup() {
-    const cards = ref[{}]
+    const cards = ref({});
+    const nomPkmn = ref('');
+    const pokemonDesc = ref({});
 
     axios
-      .get('https://pokeapi.co/api/v2/pokemon/pikachu')
+      .get('https://pokeapi.co/api/v2/pokemon?limit=100')
       .then(function (response) {
-        cards = response;
+        cards.value = response.data;
       })
       .catch(function (error) {
         console.error(error);
       });
 
+
     return {
       cards,
+      nomPkmn,
+      pokemonDesc,
     };
   },
+  computed: {
+    collectionCards() {
+      return this.cards?.['results'];
+    },
+    listeNomPkmn() {
+      return this.collectionCards?.find(card => card.name.toLowerCase() === this.nomPkmn.toLowerCase());
+    },
+    description() {
+      const pokemon = ref({});
+
+      axios
+      .get(this.listeNomPkmn?.url)
+      .then(function (response) {
+        pokemon.value = response.data;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+      this.pokemonDesc = pokemon;
+      console.log(this.pokemonDesc); 
+    }
+  }
 });
 </script>
 
