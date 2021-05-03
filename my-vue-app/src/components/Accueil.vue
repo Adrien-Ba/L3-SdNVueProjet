5bcbb24a756d808fbb5b97a02524862e7c9c9675
Adrien-Ba <adrien.bassail@live.fr>
2021-05-03 15:49:14 +0200

    merge

--- my-vue-app/src/components/Accueil.vue
+++ my-vue-app/src/components/Accueil.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
       <ul>
-        <li v-for="item in store.getters.getCollection" :key="item.id">{{ item.id }} {{item.name}} <img :src="item.sprite"/><router-link :to="{ name: 'PeuImporte', params: {id: item.id } }">more..</router-link>
-</li>
+        <li v-for="item in store.getters.getCollection" :key="item.id">{{ item.id }} {{item.name}} <img :src="item.sprite"/><router-link :to="{ name: 'peuimporte', params: {id: item.id } }">more..</router-link></li>
       </ul>
       <button @click="suivant">Suivant</button>
   </div>
@@ -18,30 +17,6 @@ export default defineComponent({
     const collection = ref([]);
     const store = useStore();
     var nbr = 0;
-
-
-   /* for(let i = 1; i < 11; i++) {
-            axios
-                  .get('https://pokeapi.co/api/v2/pokemon/'+i)
-                  .then(function (response) {
-                      const newPokemon = {id:null,name:null,sprite:null};
-                      newPokemon.sprite = response.data.sprites.other?.['official-artwork'].front_default;
-                      newPokemon.name = response.data.name;
-                      newPokemon.id= response.data.id;
-                      collection.value.push(newPokemon);
-                      
-            })
-            .catch(function (error) {
-                console.error(error);
-            });
-
-        
-        console.log(collection);
-        store.commit('ajout',newPokemon);
-        console.log("store"+store.getters.getCollection)
-      //this.type = typeTmp;
-    }*/
-
 
     return {
       collection,
